30d14f75c4a6994ea68d5b40f321e31420269433
bipin <krbipin25055@gmail.com>
2024-04-04 11:59:44 +0530

    Learned Partial  api in typescript

--- src/pick.ts
+++ src/pick.ts
@@ -10,5 +10,15 @@ interface User {
 type UpdateProps = Pick<User, 'name' | 'age' | 'password'>
 
 function updateUser(updateProps : UpdateProps){
-    // hit the database call and update the user
+    // hit the database and update the user strict arguments
 }
+
+updateUser({name:"bipin", age:23, password : "234"})
+
+type UpdatePropsOptional = Partial<UpdateProps>
+
+function updateUser2(updateProps : UpdatePropsOptional){
+    // hit the database and update the user with optional arguments
+}
+
+updateUser2( {name: "bipin"} )
