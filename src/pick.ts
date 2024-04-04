fb7510618b18470a59ee3766998cdc1704a79abf
bipin <krbipin25055@gmail.com>
2024-04-04 12:54:16 +0530

    learned type inference in zod

--- src/pick.ts
+++ src/pick.ts
@@ -132,3 +132,34 @@ const hanleEvent =(event : newEventExclued)=>{
 }
 hanleEvent('click')
 // hanleEvent('scroll') // gives scroll is not assignable error
+
+
+/* -------- Type inference in zod -------------- */
+import { z } from 'zod';
+import express from "express";
+
+const app = express();
+
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
+
+type FinalUserProfileSchema = z.infer<typeof userProfileSchema>
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody: FinalUserProfileSchema = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return
+  }
+  // update database here
+  res.json({
+    message: "User updated"
+  })
+});
+
+app.listen(3000);
